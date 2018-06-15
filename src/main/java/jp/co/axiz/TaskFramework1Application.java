4b71f383cea0a9d66738ff4f3b8f7a0a4e82dc55
admin <admin@admin-HP>
2018-06-15 15:30:11 +0900

    課題

---
+++ src/main/java/jp/co/axiz/TaskFramework1Application.java
@@ -0,0 +1,12 @@
+package jp.co.axiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFramework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFramework1Application.class, args);
+	}
+}
